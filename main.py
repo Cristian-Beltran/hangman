90810672eaf3ea4a2cf3ea7e5b993109620751ea
Cristian <cristian.beltran.paco@gmail.com>
2021-09-16 10:34:48 -0400

    Obtener una palabara de los datos

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+from random import randrange
+
+
+def random():
+    DATA = read()
+    num = randrange(len(DATA))
+    return DATA[num].upper()
+
 def read():
     DATA = []
     with open('./archivos/data.txt','r',encoding='utf-8') as r:
         DATA = [x[:-1] for x in r]
     return DATA
+
+
 def run():
-    print(read())
+    print(random())
+
+
+
+
 if __name__=='__main__':
     run()
