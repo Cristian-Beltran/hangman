61f508a577674544261d54e96bb25df4c0194f58
Cristian <cristian.beltran.paco@gmail.com>
2021-09-16 10:24:02 -0400

    Importacion de datos

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def read():
+    DATA = []
+    with open('./archivos/data.txt','r',encoding='utf-8') as r:
+        DATA = [x[:-1] for x in r]
+    return DATA
 def run():
-    pass
-
+    print(read())
 if __name__=='__main__':
     run()
