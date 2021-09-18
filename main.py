5b19caa42562f0b558c43b60cc6da2661a723702
Cristian <cristian.beltran.paco@gmail.com>
2021-09-18 15:40:30 -0400

    Version 1 del juego

--- main.py
+++ main.py
@@ -1,23 +1,107 @@
 from random import randrange
+import os
 
 
-def random():
-    DATA = read()
+
+
+def Clear():
+    """Funcion de limpieza de pantalla"""
+    os.system("clear")
+
+
+
+def Hello():
+    """Mensaje de Bienvenida al juego"""
+    print("Bienvenido al juego del ahorcado")
+    print("Presiona Enter para comenzar con el juego")
+    print("Si quieres salir presionsa Ctrl + c ")
+    input()
+
+def Game():
+    """Inicio de valores de juego"""
+    strikes = 0
+    word = Random()
+    res = []
+    for i in range(1,len(word)+1): 
+        res.append("_") 
+    Play(strikes,res,word)
+
+def Retry():
+    x = input("Seguir jugando [S/N]")
+    if x.lower() == 's':
+        Game()
+    else:
+        print("Gracias por jugar hasta luego")
+        exit()
+
+def Win(word,res):
+    string = ""
+    for letter in res:
+        string += letter 
+    if word == string:
+        print("Bien hecho acabas de ganar")
+        Retry()
+
+
+def lose():
+    print("perdiste")
+    Retry()
+
+
+def String(res):
+    """Contruccion de la palabra imprimida con espacios"""
+    string = ""
+    for i in range(0,len(res)):
+       string += f" {res[i]} "  
+    return string 
+
+
+def Play(strikes,res,word):
+    """Intentos de la partida"""
+    Clear()
+    print("Porfavor ingrese una letra")
+    print(strikes)
+    print(word)
+    print(String(res))
+    Win(word,res)
+    letter = input(":").upper() #Ingreso de letra 
+    if letter in word:
+        for i in range(0,len(word)):
+            if word[i] == letter:
+                res[i] = letter
+    else:
+        if strikes >= 7:
+            lose()
+        else:
+            strikes += 1
+
+    Play(strikes,res,word) 
+
+
+     
+
+
+def Random():
+    """Obtiene la lista de datos y selecciona de manera random"""
+    DATA = Read()
     num = randrange(len(DATA))
     return DATA[num].upper()
 
-def read():
+def Read():
+    """Lee los datos en el archivo data.txt y crea una lista"""
     DATA = []
     with open('./archivos/data.txt','r',encoding='utf-8') as r:
         DATA = [x[:-1] for x in r]
     return DATA
 
 
-def run():
-    print(random())
-
-
-
+def Run():
+    """Funcion de arranque"""
+    try:
+        Hello()
+        Game()
+    except KeyboardInterrupt:
+        print("Gracias por jugar hasta luego")
 
 if __name__=='__main__':
-    run()
+    Run()
